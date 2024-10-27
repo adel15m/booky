507f888aef486570cc7d46f79c0a8b239a985659
adel <mr.adel56m@gmail.com>
2024-10-27 18:03:42 +0300

    Add Book repository interface

---
+++ backend/src/main/java/com/example/booky/repository/BookRepository.java
@@ -0,0 +1,12 @@
+package com.example.booky.repository;
+
+import com.example.booky.Entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByIsbn(String isbn);
+}
