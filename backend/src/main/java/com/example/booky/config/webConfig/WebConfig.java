3dbcc7739d14454c122bf2b27a746acdcbf949d5
adel <mr.adel56m@gmail.com>
2024-10-31 09:23:54 +0300

    configure and added a Dockerfiles to run in Docker Compose

--- backend/src/main/java/com/example/booky/config/webConfig/WebConfig.java
+++ backend/src/main/java/com/example/booky/config/webConfig/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("http://localhost:80")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Accept")
                 .allowCredentials(true);
